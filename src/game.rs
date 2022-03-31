e9d25c2f9dfdbe9b7d8447182589c1517ee3c546
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2022-03-31 22:14:01 +0200

    pass canvas down to other class and store it in it

--- src/game.rs
+++ src/game.rs
@@ -1,19 +1,28 @@
 use sdl2::pixels::Color;
+use sdl2::rect::{Point, Rect};
 use sdl2::render::WindowCanvas;
 
-pub struct Game {}
+pub struct Game<'canvas> {
+    canvas: &'canvas mut WindowCanvas,
+}
 
-impl Game {
+impl<'canvas> Game<'canvas> {
     pub fn new(canvas: &mut WindowCanvas) -> Game {
-        canvas.set_draw_color(Color::RGB(0, 255, 255));
-        canvas.clear();
-        canvas.present();
-        Game {}
+        Game { canvas }
     }
-    pub fn draw(self: &Self, canvas: &mut WindowCanvas, i: &mut u8) {
-        *i = (*i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(*i, 64, 255 - *i));
-        canvas.clear();
-        canvas.present();
+    pub fn draw_background(self: &mut Self) {
+        self.canvas.set_draw_color(Color::RGB(67, 139, 220));
+        self.canvas.clear();
+    }
+
+    pub fn draw_player(self: &mut Self) {
+        self.canvas.set_draw_color(Color::RGB(67, 220, 139));
+        self.canvas.fill_rect(Rect::new(10, 10, 140, 140)).unwrap();
+    }
+
+    pub fn draw(self: &mut Self) {
+        self.draw_background();
+        self.draw_player();
+        self.canvas.present();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -21,8 +21,7 @@ pub fn main() {
     let mut canvas = window.into_canvas().build().unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
 
-    let mut i = 0;
-    let game = Game::new(&mut canvas);
+    let mut game = Game::new(&mut canvas);
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -36,7 +35,7 @@ pub fn main() {
             }
         }
         // The rest of the game loop goes here...
-        game.draw(&mut canvas, &mut i);
+        game.draw();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
