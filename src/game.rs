02eb88409b0e58cd0bd14975f579b4f9be74c08f
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2022-04-04 23:13:45 +0200

    just bring the code inside a special struct

--- src/game.rs
+++ src/game.rs
@@ -1,15 +1,51 @@
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use sdl2::rect::{Point, Rect};
+use sdl2::rect::Rect;
 use sdl2::render::WindowCanvas;
+use sdl2::Sdl;
+use std::time::Duration;
 
-pub struct Game<'canvas> {
-    canvas: &'canvas mut WindowCanvas,
+pub struct Game<'sdl> {
+    sdl: &'sdl mut Sdl,
+    canvas: WindowCanvas,
 }
 
-impl<'canvas> Game<'canvas> {
-    pub fn new(canvas: &mut WindowCanvas) -> Game {
-        Game { canvas }
+impl<'sdl> Game<'sdl> {
+    pub fn new(sdl: &mut Sdl) -> Game {
+        let video_subsystem = sdl.video().unwrap();
+
+        let window = video_subsystem
+            .window("rust-sdl2 demo", 800, 600)
+            .position_centered()
+            .build()
+            .unwrap();
+
+        let canvas = window.into_canvas().build().unwrap();
+
+        Game { sdl, canvas }
     }
+
+    pub fn start(self: &mut Self) {
+        let mut event_pump = self.sdl.event_pump().unwrap();
+
+        'running: loop {
+            for event in event_pump.poll_iter() {
+                match event {
+                    Event::Quit { .. }
+                    | Event::KeyDown {
+                        keycode: Some(Keycode::Escape),
+                        ..
+                    } => break 'running,
+                    _ => {}
+                }
+            }
+            // The rest of the game loop goes here...
+            self.draw();
+            ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        }
+    }
+
     pub fn draw_background(self: &mut Self) {
         self.canvas.set_draw_color(Color::RGB(67, 139, 220));
         self.canvas.clear();
--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,10 @@ extern crate sdl2;
 pub mod game;
 
 use crate::game::Game;
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
-use std::time::Duration;
 
 pub fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+    let mut sdl = sdl2::init().unwrap();
 
-    let window = video_subsystem
-        .window("rust-sdl2 demo", 800, 600)
-        .position_centered()
-        .build()
-        .unwrap();
-
-    let mut canvas = window.into_canvas().build().unwrap();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-
-    let mut game = Game::new(&mut canvas);
-
-    'running: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
-            }
-        }
-        // The rest of the game loop goes here...
-        game.draw();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    }
+    let mut game = Game::new(&mut sdl);
+    game.start();
 }
